2a73c8070cc22a82478bd7a1367a0d5eecbb78ab
YERIEL-RYU <yeriel.ryu@gmail.com>
2022-12-07 00:59:47 +0900

    8주차 실습

---
+++ 4-2/C#/8주차/EX21/EX21/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public void setNameAndScore(string name, int score)
+        {
+            label1.Text = name + "님의 성적 합계는" + score + "입니다.";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
